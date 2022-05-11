34ab5c696745dabb5e6c404daaa8f00efbae8b36
Acer <elmirbekalisherev@gmail.com>
2022-05-11 22:04:31 +0600

    spring-bean

---
+++ src/main/java/peaksoft/App.java
@@ -0,0 +1,28 @@
+package peaksoft;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main( String[] args ) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+
+        Cat cat1 =
+                applicationContext.getBean("cat", Cat.class);
+        Cat cat2 =
+                applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(cat1);
+        System.out.println(cat1 == cat2);
+    }
+}
